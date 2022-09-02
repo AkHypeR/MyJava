f46aa39c27b8d6b47d8340e27bc9557a47a31a72
AkHyper <akashroy.9126@gmail.com>
2022-09-02 20:25:00 +0530

    my first commit

--- condition.java
+++ condition.java
@@ -1,5 +1,8 @@
 public class condition {
     public static void main(String[] args) {
         System.out.println("hello world");
+        for(int i=0;i<7;i++){
+            System.out.println(i);
+        }
     }
 }
