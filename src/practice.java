6f37ef52c0cfc2ed618f90cd3ff85435f4416c60
AkHyper <akashroy.9126@gmail.com>
2022-09-02 21:09:24 +0530

    my first commit

---
+++ src/practice.java
@@ -0,0 +1,14 @@
+public class practice {
+    public static void isEvenNumber(int n) {
+        if (n % 2 == 0) {
+            System.out.println(n + " is Even Number ");
+        } else {
+            System.out.println(n + " is odd Number ");
+        }
+    }
+
+    public static void main(String[] args) {
+       isEvenNumber(5);
+
+    }
+}
